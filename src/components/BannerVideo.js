bbba21a98f345351a8512e6bbd4044b494bdaa7f
bananana0118 <bananana0118@gmail.com>
2024-08-02 20:30:08 +0900

    feat : 충돌 라이브러리 : 코드 preload 를 위해 웹팩 관련 라이브러리를 설치할떄부터 충돌나서 걍 되돌림

--- src/components/BannerVideo.js
+++ src/components/BannerVideo.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import video from '../assets/banner-video.mp4'
+import React, { useState, useEffect } from "react";
+import video from "../assets/banner-video.mp4";
+import FontFaceObserver from "fontfaceobserver";
 
 function BannerVideo() {
-	return (
-		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
-			<div className="absolute h-screen w-full left-1/2">
-				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
-			</div>
-			<div className="w-full h-full flex justify-center items-center">
-				<div className="text-white text-center">
-					<div className="text-6xl leading-none font-semibold">KEEP</div>
-					<div className="text-6xl leading-none font-semibold">CALM</div>
-					<div className="text-3xl leading-loose">AND</div>
-					<div className="text-6xl leading-none font-semibold">RIDE</div>
-					<div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
-				</div>
-			</div>
-		</div>
-	)
+    const [isFontLoaded, setIsFontLoaded] = useState();
+
+    useEffect(() => {
+        const font = new FontFaceObserver("BMYEONSUNG");
+        font.load().then(function () {
+            console.log("폰트로드");
+            setIsFontLoaded(true)
+        });
+    }, []);
+
+    return (
+        <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
+            <div className="absolute h-screen w-full left-1/2">
+                <video
+                    src={video}
+                    className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
+                    autoPlay
+                    loop
+                    muted
+                >
+                    <source src={video} type="video/mp4"></source>
+                </video>
+            </div>
+            <div
+                className="w-full h-full flex justify-center items-center"
+                style={{
+                    opacity: isFontLoaded ? 1 : 0,
+                    transition: "opacity 0.3s ease",
+                }}
+            >
+                <div className="text-white text-center">
+                    <div className="text-6xl leading-none font-semibold">
+                        KEEP
+                    </div>
+                    <div className="text-6xl leading-none font-semibold">
+                        CALM
+                    </div>
+                    <div className="text-3xl leading-loose">AND</div>
+                    <div className="text-6xl leading-none font-semibold">
+                        RIDE
+                    </div>
+                    <div className="text-5xl leading-tight font-semibold">
+                        LONGBOARD
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default BannerVideo
+export default BannerVideo;
